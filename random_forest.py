ebbf228bd6c7f74ef05d154aa9bc0c75d5eb1819
Noory Kim <noorykim@users.noreply.github.com>
2017-11-08 16:39:27 -0500

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -30,3 +30,12 @@ print(my_forest.score(features_forest, target))
 test_features = test[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
 pred_forest = my_forest.predict(test_features)
 print(len(pred_forest))
+
+
+#Request and print the `.feature_importances_` attribute
+print(my_tree_two.feature_importances_)
+print(my_forest.feature_importances_)
+
+#Compute and print the mean accuracy score for both models
+print(my_tree_two.score(features_two, target))
+print(my_forest.score(features_two, target))
