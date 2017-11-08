d3abb926e22ad4618c8438f58a40b2a007478a91
Noory Kim <noorykim@users.noreply.github.com>
2017-11-08 16:09:45 -0500

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -1,14 +1,17 @@
 # Decision Trees 
 # Source : https://campus.datacamp.com/courses/kaggle-python-tutorial-on-machine-learning/predicting-with-decision-trees
 
-# Import the Numpy library
+
+# You will use the scikit-learn and numpy libraries to build your first decision tree. 
+# scikit-learn can be used to create tree objects from the DecisionTreeClassifier class. 
+# The methods that we will use take numpy arrays as inputs 
+# and therefore we will need to create those from the DataFrame that we already have. 
+# We will need the following to build a decision tree
+# - target: A one-dimensional numpy array containing the target/response from the train data. (Survival in your case)
+# - features: A multidimensional numpy array containing the features/predictors from the train data. (ex. Sex, Age)
+
 import numpy as np
-
-# Import 'tree' from scikit-learn library
-from sklearn import tree
-
-# Print the train data to see the available features
-print(train)
+from sklearn import tree    # Import 'tree' from scikit-learn library
 
 # Create the target and features numpy arrays: target, features_one
 target = train["Survived"].values
@@ -18,7 +21,9 @@ features_one = train[["Pclass", "Sex", "Age", "Fare"]].values
 my_tree_one = tree.DecisionTreeClassifier()
 my_tree_one = my_tree_one.fit(features_one, target)
 
-# Look at the importance and score of the included features
+# One way to quickly see the result of your decision tree is to see the importance of the features that are included. 
+# This is done by requesting the .feature_importances_ attribute of your tree object. 
+# Another quick metric is the mean accuracy that you can compute using the .score() function with features_one and target as arguments.
 print(my_tree_one.feature_importances_)
 print(my_tree_one.score(features_one, target))
 
