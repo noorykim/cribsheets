36550af08f146c443534a29e8d27ba52f8e88038
Noory Kim <noorykim@users.noreply.github.com>
2017-11-08 16:20:01 -0500

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -15,3 +15,18 @@
 # Use RandomForestClassifier() class instead of the DecisionTreeClassifier() class.
 # n_estimators needs to be set when using the RandomForestClassifier() class. 
 # This argument allows you to set the number of trees you wish to plant and average over.
+
+from sklearn.ensemble import RandomForestClassifier
+features_forest = train[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
+
+# Building and fitting my_forest
+forest = RandomForestClassifier(max_depth = 10, min_samples_split=2, n_estimators = 100, random_state = 1)
+my_forest = forest.fit(features_forest, target)
+
+# Print the score of the fitted random forest
+print(my_forest.score(features_forest, target))
+
+# Compute predictions on our test set features then print the length of the prediction vector
+test_features = test[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
+pred_forest = my_forest.predict(test_features)
+print(len(pred_forest))
