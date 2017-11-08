f455e0ac94a33f361647efad4fb7e2dd3fce5047
Noory Kim <noorykim@users.noreply.github.com>
2017-11-08 16:00:28 -0500

    Create decision_tree.py

---
+++ decision_tree.py
@@ -0,0 +1,58 @@
+# Decision Trees 
+# Source : https://campus.datacamp.com/courses/kaggle-python-tutorial-on-machine-learning/predicting-with-decision-trees?ex=6
+
+# Import the Numpy library
+import numpy as np
+
+# Import 'tree' from scikit-learn library
+from sklearn import tree
+
+# Print the train data to see the available features
+print(train)
+
+# Create the target and features numpy arrays: target, features_one
+target = train["Survived"].values
+features_one = train[["Pclass", "Sex", "Age", "Fare"]].values
+
+# Fit your first decision tree: my_tree_one
+my_tree_one = tree.DecisionTreeClassifier()
+my_tree_one = my_tree_one.fit(features_one, target)
+
+# Look at the importance and score of the included features
+print(my_tree_one.feature_importances_)
+print(my_tree_one.score(features_one, target))
+
+
+# Impute the missing value with the median
+test.Fare[152] = test.Fare.median()
+print(test.Fare[152])
+
+# Extract the features from the test set: Pclass, Sex, Age, and Fare.
+test_features = test[['Pclass', 'Sex', 'Age', 'Fare']].values
+
+# Make your prediction using the test set
+my_prediction = my_tree_one.predict(test_features)
+
+# Create a data frame with two columns: PassengerId & Survived. Survived contains your predictions
+PassengerId =np.array(test["PassengerId"]).astype(int)
+my_solution = pd.DataFrame(my_prediction, PassengerId, columns = ["Survived"])
+print(my_solution)
+
+# Check that your data frame has 418 entries
+print(my_solution.shape)
+
+# Write your solution to a csv file with the name my_solution.csv
+my_solution.to_csv("my_solution_one.csv", index_label = ["PassengerId"])
+
+
+# Create a new array with the added features: features_two
+features_two = train[["Pclass","Age","Sex","Fare", "SibSp", "Parch", "Embarked"]].values
+
+#Control overfitting by setting "max_depth" to 10 and "min_samples_split" to 5 : my_tree_two
+max_depth = 10
+min_samples_split = 5
+my_tree_two = tree.DecisionTreeClassifier(max_depth = 10, min_samples_split = 5, random_state = 1)
+my_tree_two = my_tree_two.fit(features_two, target)
+
+#Print the score of the new decison tree
+print(my_tree_two.score(features_two, target))
