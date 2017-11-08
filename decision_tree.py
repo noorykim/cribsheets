323975bee18baf4969a333fc9b955b14370a1557
Noory Kim <noorykim@users.noreply.github.com>
2017-11-08 16:15:40 -0500

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -84,3 +84,29 @@ my_tree_two = my_tree_two.fit(features_two, target)
 
 #Print the score of the new decison tree
 print(my_tree_two.score(features_two, target))
+
+
+
+
+# Feature-engineering = adding a derived variable
+# Create your own new predictive attribute: family_size.
+# A valid assumption is that larger families need more time to get together on a sinking ship, 
+# and hence have lower probability of surviving. 
+# Family size is determined by the variables SibSp and Parch, which indicate the number of family members 
+# a certain passenger is traveling with. 
+# So when doing feature engineering, you add a new variable family_size, which is the sum of SibSp and Parch plus one 
+# (the observation itself), to the test and train set.
+
+# Create train_two with the newly defined feature
+train_two = train.copy()
+train_two["family_size"] = train["SibSp"] + train["Parch"] + 1
+
+# Create a new feature set and add the new feature
+features_three = train_two[["Pclass", "Sex", "Age", "Fare", "SibSp", "Parch", "family_size"]].values
+
+# Define the tree classifier, then fit the model
+my_tree_three = tree.DecisionTreeClassifier()
+my_tree_three = my_tree_three.fit(features_three, target)
+
+# Print the score of this decision tree
+print(my_tree_three.score(features_three, target))
